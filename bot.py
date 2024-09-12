edc5b20662fae4405a0ef6a5b9a5219e85eafde8
2Dkun1355 <oliynikbogdan.ua@gmail.com>
2024-09-12 18:06:16 +0300

    add button

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import json
-
 import telebot
+from telebot import types
 import requests
+from config import token
 
-bot = telebot.TeleBot(token='7144823259:AAEborHn6X6yFNNOAwn4QZZylE3vhyEMh4w')
+bot = telebot.TeleBot(token=token)
 API_URL = 'http://127.0.0.1:8000'
-
 
 def auth(message):
     url = f'{API_URL}/auth/users/'
@@ -24,30 +24,53 @@ def auth(message):
         }
         response = requests.post(url=url_next, data=data_next)
 
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    auth(message)
-    # додати кнопки "Знайти" та "Тест"
-    # зареєструвати наступний крок menu
+    # auth(message)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(
+        types.KeyboardButton("Знайти"),
+        types.KeyboardButton("Тест"),
+    )
 
-def menu(message, current_bot):
-    pass
+    bot.send_message(message.chat.id, "Виберіть опцію:", reply_markup=markup)
 
-def process_search(message, current_bot):
-    # вивести інформаційне повідомлення "Введіть параметри пошуку..."
-    # зареєструвати наступний крок search_vacancies
-    pass
+@bot.message_handler(func=lambda message: True)
+def menu(message):
+    if message.text == 'Знайти':
+        process_search(message)
+    elif message.text == 'Тест':
+        process_test(message)
 
-def search_vacancies(message, current_bot):
+def process_search(message):
+    bot.send_message(message.chat.id, f"Введіть параметри пошуку в форматі:\n"
+                                      f"Мова програмування\n"
+                                      f"Рівень\n"
+                                      f"Місто чи віддалено")
+    bot.register_next_step_handler(message, search_vacancies)
+
+def search_vacancies(message):
     # зчитує параметри які ввів користувач  розбиває на окремі змінні
     # робить запит на АПІ з потрібними фільтрами
     # отримує відповідь з АПІ і формує текстове повідомлення з вакансіями та надсилає його юзеру
+    try:
+        text = message.text.split()
+        data = {'programming_language': text[0],
+                'level_need': text[1],
+                }
+        if text[2].lower() == 'віддалено':
+            data.update({
+                'is_remote': True
+            })
+        else:
+            data.update({
+                'location': text[2]
+            })
+    except:
+        pass
 
-def process_test(message, current_bot):
-    pass
-
-
+def process_test(message):
+    bot.send_message(message.chat.id, "Тестова кнопка, просто щоб була :)")
 
 if __name__ == '__main__':
     bot.polling()
